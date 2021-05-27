4dc5ae756b47e484a0c955538976466a9dc9ecab
darknet <nkrezeski1@gmail.com>
2021-05-27 22:25:32 +0200

    Remove redundant lines

--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -1,11 +1,6 @@
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
